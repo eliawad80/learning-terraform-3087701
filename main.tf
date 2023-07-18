e09ffb6a6fd5eebbadec28839858575c56dec2c1
Elias Awad <info@innocorner.com>
2023-07-18 19:20:44 +0200

    load balancer correction blog vpc id

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-data "aws_ami" "app_ami" {
+able_data "aws_ami" "app_ami" {
   most_recent = true
 
   filter {
@@ -21,12 +21,13 @@ data "aws_ami" "app_ami" {
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "vpc"
+  name = "dev"
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-12b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
+  enable_nat_gateaway = true
 
   tags = {
     Terraform = "true"
@@ -93,7 +94,8 @@ module "blog_sg" {
   version = "5.1.0"
 
   name = "blog"
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.vpc.public_subnets[0]
+
   ingress_rules = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules = ["all-all"]
