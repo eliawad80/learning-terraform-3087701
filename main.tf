71ca286edcfb86a18bb5a4b0c6a8ee7d8df6c490
Elias Awad <info@innocorner.com>
2023-07-19 17:25:39 +0200

    using modules for security group
    correcting some typo errors

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [modules.blog_sg.security_group_id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
   #vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
@@ -34,13 +34,13 @@ module "blog_sg" {
   version = "5.1.0"
   name = "blog_new"
   
-vpc_id      = data.aws_vpc.default.id
+  vpc_id      = data.aws_vpc.default.id
 
-ingress_rules = ["http-80-tcp","https-443-tcp"]
-ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
 
-egress_rules = ["all-all"]
-egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 
 }
 
