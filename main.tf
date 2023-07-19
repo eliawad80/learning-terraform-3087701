5407848c146c4f104d7fa21ef6cbdffb31411d8e
Elias Awad <info@innocorner.com>
2023-07-20 01:45:02 +0200

    Load balancer from module

--- main.tf
+++ main.tf
@@ -50,10 +50,68 @@ resource "aws_instance" "blog" {
     Name = "HelloWorld"
   }
 }
+
+module "blog-alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 8.0"
+
+  name = "my-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = module_blog_sg.security_group_id
+
+  #access_logs = {
+  #  bucket = "my-alb-logs"
+  #}
+
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+        #my_other_target = {
+        #  target_id = "i-a1b2c3d4e5f6g7h8i"
+        #  port = 8080
+        #}
+      }
+    }
+  ]
+
+  #https_listeners = [
+  #  {
+  #    port               = 443
+  #    protocol           = "HTTPS"
+  #    certificate_arn    = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+  #    target_group_index = 0
+  #  }
+  #]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
-  name = "blog_new"
+  name = "blog"
   
   #vpc_id      = data.aws_vpc.default.id
   # the above line to use with the default VPC in line 17 and been replaced with the line below to be used with VPC module
