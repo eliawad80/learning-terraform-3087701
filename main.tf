e334b81bef30d76c1b4df4316338a2042657ac06
Elias Awad <info@innocorner.com>
2023-07-20 02:07:26 +0200

    load balancer alb fixed

--- main.tf
+++ main.tf
@@ -41,6 +41,7 @@ module "vpc" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+  
   vpc_id             = module.blog_vpc.vpc_id
   subnet_id     = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
