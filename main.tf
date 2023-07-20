2c41d409d1d0e5b50d242decfb41f0f1cb4e182d
Elias Awad <info@innocorner.com>
2023-07-20 02:28:05 +0200

    fixing load balancer alb

--- main.tf
+++ main.tf
@@ -51,11 +51,11 @@ resource "aws_instance" "blog" {
   }
 }
 
-module "blog-alb" {
+module "alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
-  name = "my-alb"
+  name = "blog-alb"
 
   load_balancer_type = "application"
 
