b8eaddb3105341e7f3805be91ee6a24fff364772
Elias Awad <info@innocorner.com>
2023-07-20 14:22:57 +0200

    autoscaling module autosc

--- main.tf
+++ main.tf
@@ -38,20 +38,34 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+#resource "aws_instance" "blog" {
+#  ami           = data.aws_ami.app_ami.id
+#  instance_type = var.instance_type
 
-  subnet_id     = module.blog_vpc.public_subnets[0]
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-  #vpc_security_group_ids = [aws_security_group.blog.id]
+# subnet_id     = module.blog_vpc.public_subnets[0]
+#  vpc_security_group_ids = [module.blog_sg.security_group_id]
+#  #vpc_security_group_ids = [aws_security_group.blog.id]
 
-  tags = {
-    Name = "HelloWorld"
-  }
+#  tags = {
+#    Name = "HelloWorld"
+#  }
+#}
+# we replaced the resource instance with the autosc module below and we comment/deletewhats in  the output file
+
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.10.0"
+  name = "blog"
+  min_size = 1
+  max_size = 2
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_grop_arns
+  security_groups = [module.blog_sg.security_group_id]
+  image_id       = data.aws_ami.app_ami.id
+  instance_type  = var.instance_type
 }
 
-module "alb" {
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
